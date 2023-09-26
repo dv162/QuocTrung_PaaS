ca95cba883141af7a9e31970ec6cca2aa148c11f
QuocTrungdv <107343030+dv162@users.noreply.github.com>
2023-09-26 10:11:39 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP Example</title>
+</head>
+<body>
+    <?php
+    $message = "Hello, World!";
+    echo "<h1>$message</h1>";
+    ?>
+    <p>This is a paragraph in HTML.</p>
+</body>
+</html>
